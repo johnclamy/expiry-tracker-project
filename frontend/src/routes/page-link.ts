9b09009d34e353002878622b57834ece5de7c35b
johnclamy <johnclamy@gmail.com>
2026-07-12 20:57:28 +0100

    define the constants for the nav links

---
+++ frontend/src/routes/page-link.ts
@@ -0,0 +1,19 @@
+interface PageLink {
+    HOME: string;
+    CAREERS: string;
+    JOBS: string;
+    ABOUT: string;
+    CONTACT: string;
+}
+
+
+const pageLink: PageLink = {
+    HOME: '/',
+    CAREERS: '/ai-careers',
+    JOBS: '/new-jobs',
+    ABOUT: '/about',
+    CONTACT: '/contact'
+}
+
+
+export default pageLink
