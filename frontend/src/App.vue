5995c56dc1bc5ba446950adce6ae2b2600d61f6a
johnclamy <johnclamy@gmail.com>
2026-07-12 01:58:29 +0100

    import the RouterView component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h3>Testing the root component...</h3>
+  <RouterView />
+  <!-- credit for logo icon image -->
+  <!-- <a href="https://www.flaticon.com/free-icons/time-tracking" title="time tracking icons">Time tracking icons created by vectorsmarket15 - Flaticon</a> -->
 </template>
 
 <style scoped></style>
+
