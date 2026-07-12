7d4713fe6059476c1da2d3232426b50c5a1da4e2
johnclamy <johnclamy@gmail.com>
2026-07-12 20:58:40 +0100

    define the global header component

---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header class="bg-white">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <!-- logo section -->
+                <section class="md:flex md:items-center md:gap-12">
+                    <RouterLink class="block text-teal-600" to='/'>
+                        <span class="sr-only">Home</span>
+                        <img class="h-8 w-auto object-contain" src="../../assets/logo.png" alt="logo" />
+                    </RouterLink>
+                </section>
+                <!-- navigation section -->
+                <section class="hidden md:block">
+                    <nav aria-label="Global">
+                        <ul class="flex items-center gap-6 text-sm">
+                            <li>
+                                <RouterLink class="text-gray-500 transition hover:text-gray-500/75" to='/'> Home </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="text-gray-500 transition hover:text-gray-500/75" to='/ai-careers'> AI Careers </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="text-gray-500 transition hover:text-gray-500/75" to='/new-jobs'> New Jobs </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="text-gray-500 transition hover:text-gray-500/75" to='/about'> About </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="text-gray-500 transition hover:text-gray-500/75" to='/contact'> Contact </RouterLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <RouterView />
+                </section>
+                <!-- Login/ Register section -->
+                <section class="flex items-center gap-4">
+                    <div class="sm:flex sm:gap-4">
+                        <a class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm" href="#">
+                            Login
+                        </a>
+                        <div class="hidden sm:flex">
+                            <a class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600" href="#">
+                                Register
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Hamburger menu -->
+                    <div class="block md:hidden">
+                        <button class="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                            <span class="sr-only">Toggle menu</span>
+                            <svg
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="size-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-width="2"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </header>
+</template>
