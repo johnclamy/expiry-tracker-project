54c01373835656b9acedd4dac61b1960375d8405
johnclamy <johnclamy@gmail.com>
2026-07-12 21:00:03 +0100

    configure the router

---
+++ frontend/src/routes/router.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import pageLink from './page-link'
+
+// Import all view components
+import HomeView from '../components/views/HomeView.vue'
+import AICareerView from '../components/views/AICareerView.vue'
+import NewJobsView from '../components/views/NewJobsView.vue'
+import AboutView from '../components/views/AboutView.vue'
+import ContactView from '../components/views/ContactView.vue'
+
+
+const routes: RouteRecordRaw[] = [
+    { path: pageLink.HOME, component: HomeView },
+    { path: pageLink.CAREERS, component: AICareerView },
+    { path: pageLink.JOBS, component: NewJobsView },
+    { path: pageLink.ABOUT, component: AboutView },
+    { path: pageLink.CONTACT, component: ContactView },
+]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+
+export default router
