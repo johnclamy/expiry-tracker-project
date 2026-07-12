9e3fde5bb07284d7ae341175b662dab6341a98c3
johnclamy <johnclamy@gmail.com>
2026-07-12 21:13:01 +0100

    move the RouterView to within the nav tag in the Header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-  import { RouterView } from 'vue-router'
+  import Header from './components/layout/Header.vue'
 </script>
 
 <template>
-  <RouterView />
-  <!-- credit for logo icon image -->
-  <!-- <a href="https://www.flaticon.com/free-icons/time-tracking" title="time tracking icons">Time tracking icons created by vectorsmarket15 - Flaticon</a> -->
+    <Header />
+    
+    <footer>
+    <!-- credit for logo icon image -->
+    <!-- <a href="https://www.flaticon.com/free-icons/time-tracking" title="time tracking icons">Time tracking icons created by vectorsmarket15 - Flaticon</a> -->
+    </footer>
 </template>
 
 <style scoped></style>
