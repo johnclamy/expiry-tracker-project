13eba818c8b4b311f8d8b0af30e09911601745eb
johnclamy <johnclamy@gmail.com>
2026-07-12 21:17:01 +0100

    enable the router in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
 import './assets/css/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes/router.ts'
 
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
