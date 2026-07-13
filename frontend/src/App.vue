2585383d34aa8cbd2a7ef56d171cadd998517efa
johnclamy <johnclamy@gmail.com>
2026-07-13 09:23:25 +0100

    align the main content width with the header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,19 @@
 </script>
 
 <template>
-    <Header />
-    <RouterView />
-    <footer>
-    <!-- credit for logo icon image -->
-    <!-- <a href="https://www.flaticon.com/free-icons/time-tracking" title="time tracking icons">Time tracking icons created by vectorsmarket15 - Flaticon</a> -->
-    </footer>
+    <!-- min-h-screen ensures the background spans the entire vertical viewport -->
+    <div class="bg-slate-50 text-slate-900 min-h-screen antialiased">
+        <Header />
+        <div class="mx-2">
+        <main class="bg-white mt-2 mb-8 mx-auto max-w-5xl px-2 py-2 sm:px-6 lg:px-8">
+            <RouterView />
+        </main>
+        </div>
+        <footer>
+        <!-- credit for logo icon image -->
+        <!-- <a href="https://www.flaticon.com/free-icons/time-tracking" title="time tracking icons">Time tracking icons created by vectorsmarket15 - Flaticon</a> -->
+        </footer>
+    </div>
 </template>
 
 <style scoped></style>
