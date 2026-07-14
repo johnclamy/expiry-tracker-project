97f99e4c36e9b74af3808fa844fe2345fd373138
johnclamy <johnclamy@gmail.com>
2026-07-14 22:55:28 +0100

    remove unwated template views

--- frontend/src/components/views/ContactView.vue
+++
@@ -1,4 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-    <h1>Here is where the contact form wil go.</h1>
-</template>
--- frontend/src/components/views/NewJobsView.vue
+++
@@ -1,4 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-    <h1>This pages focuses on the new jobs created by AI.</h1>
-</template>
