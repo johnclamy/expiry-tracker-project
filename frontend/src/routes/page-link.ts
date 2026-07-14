6cb49774e358c9dd18e340cb1f773f0dbb298b11
johnclamy <johnclamy@gmail.com>
2026-07-14 22:57:12 +0100

    remove the unwated page links

--- frontend/src/routes/page-link.ts
+++ frontend/src/routes/page-link.ts
@@ -1,18 +1,12 @@
 interface PageLink {
     HOME: string;
-    CAREERS: string;
-    JOBS: string;
     ABOUT: string;
-    CONTACT: string;
 }
 
 
 const pageLink: PageLink = {
     HOME: '/',
-    CAREERS: '/ai-careers',
-    JOBS: '/new-jobs',
     ABOUT: '/about',
-    CONTACT: '/contact'
 }
 
 
