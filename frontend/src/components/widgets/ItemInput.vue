096051359c4e9bf5205bcbd4e5d0f5ab444ca3c4
johnclamy <johnclamy@gmail.com>
2026-07-15 22:22:38 +0100

    replace the textarea with an input element

--- frontend/src/components/widgets/ItemInput.vue
+++ frontend/src/components/widgets/ItemInput.vue
@@ -1,34 +1,35 @@
 <script setup lang="ts">
-  const createItem = () => {
-    console.log("Creating a new item...");
-  };
 </script>
 <template>    
     <section class="mx-auto max-w-md space-y-4 border border-gray-300 bg-gray-100 p-6 rounded-none lg:max-w-full lg:flex lg:items-end lg:gap-4 lg:space-y-0">
         
         <div class="flex-1">
-            <label class="block text-sm font-medium text-gray-900" for="item-input">
+            <label class="block text-sm font-medium text-gray-900" for="item-name">
                 Item Name
             </label>
-            <textarea
-                id="item-input"
-                rows="2"
+            <input
+                id="item-name"
+                type="text"
                 class="mt-1 block w-full resize-none bg-white p-3 border border-gray-300 focus:border-indigo-500 focus:outline-none rounded-none"
-                placeholder="Enter the name and/or details of the item to be tracked"
-            ></textarea>
+                placeholder="Enter the name of the item to be tracked"
+                v-model="itemName"
+            />
         </div>
 
         <div class="flex-1">
-            <label class="block text-sm font-medium text-gray-900" for="email">
-                Email
+            <label class="block text-sm font-medium text-gray-900" for="expiry-date">
+                Expiry Date
             </label>
             <div class="mt-1">
                 <VueDatePicker
-                    id="email"
+                    id="expiry-date"
                     class="w-full bg-white border border-gray-300 bg-white focus:border-indigo-500 focus:outline-none rounded-none"
                     :enable-time-picker="false"
                     :min-date="new Date()"
+                    :max-date=tenYears
+                    :flow="flow"
                     now-button-label="Today"
+                    v-model="expiryDate"
                 />
             </div>
         </div>
@@ -36,7 +37,8 @@
         <button
             type="button"
             class="block w-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-indigo-600 rounded-lg lg:h-[50px] lg:w-auto lg:shrink-0"
-        >
+            @click="addItem"
+            >
             Add Item
         </button>        
     </section>
