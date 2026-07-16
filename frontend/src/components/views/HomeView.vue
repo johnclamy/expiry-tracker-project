245506d117da94f82755c19088ed5b6e4f603712
johnclamy <johnclamy@gmail.com>
2026-07-16 14:13:00 +0100

    replace the input field with the modal btn

--- frontend/src/components/views/HomeView.vue
+++ frontend/src/components/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-    import ItemInput from '../widgets/ItemInput.vue'
+    import ModalButton from '../widgets/ModalButton.vue'
 </script>
 <template>
-    <ItemInput />
+    <section class="min-h-screen flex flex-col bg-gradient-to-br from-amber-50 to-orange-100 p-6">
+        <ModalButton />
+    </section>
 </template>
