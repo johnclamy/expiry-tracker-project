8d880cc336d0951199f4db02b02673ad90f20a75
johnclamy <johnclamy@gmail.com>
2026-07-16 16:09:25 +0100

    add oh-vue-icons into main.js

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,33 @@ import './assets/css/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/router.ts'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { 
+      IoDocumentTextOutline, 
+      IoCalendarOutline, 
+      IoTrashOutline, 
+      IoAddCircleOutline, 
+      IoHomeOutline,
+      IoTimeOutline,
+      IoCardOutline,
+      IoPersonOutline
+    } from 'oh-vue-icons/icons'
+
+
+// --- register icons ---
+addIcons(
+    IoDocumentTextOutline,
+    IoCalendarOutline,
+    IoTrashOutline,
+    IoAddCircleOutline,
+    IoHomeOutline,
+    IoTimeOutline,
+    IoCardOutline,
+    IoPersonOutline)
 
 
 const app = createApp(App)
 
 app.use(router)
+app.component('oh-vue-icon', OhVueIcon)
 app.mount('#app')
